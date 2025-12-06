481fa442899027ecc65fb101844e6a42f9bb96bb
cdpfernando <ferpacdp@gmail.com>
2025-12-06 13:15:26 -0300

    feat: adiciona splash screen

--- app/src/main/java/br/com/conradowho/abastececerto/MainActivity.kt
+++ app/src/main/java/br/com/conradowho/abastececerto/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import br.com.conradowho.abastececerto.ResultActivity.Companion.EXTRA_CALCULATION_RESULT
@@ -52,6 +53,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
 
         db = DatabaseHandler.getInstance(this)
